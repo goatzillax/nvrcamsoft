ab9cbaeb81033e4312af56e94c1bf085218d0414
anon <anon@anon>
2024-05-09 04:29:53 -0500

    break up url/host

--- nvr-ffmpeg.sh
+++ nvr-ffmpeg.sh
@@ -3,7 +3,8 @@
 SCRIPTDIR=`dirname $0`
 source $SCRIPTDIR/env.sh
 
-HOST=$1
+URL=$1
+HOST=$2
 SEGTIME=300
 SHUTUP=-nostats
 
@@ -12,5 +13,5 @@ if [ ! -d ${RAMDIR} ]; then
 fi
 
 while true; do
-   nice -n -10 ffmpeg ${SHUTUP} -stimeout 5000000 -y -i rtsp://${HOST}/live0 -c copy -an -f segment -segment_time ${SEGTIME} -segment_atclocktime 1 -reset_timestamps 1 -strftime 1 -segment_format mp4 "${RAMDIR}/${HOST}-%Y%m%d-%H%M%S.mp4"
+   nice -n -10 ffmpeg ${SHUTUP} -stimeout 5000000 -y -i rtsp://${URL} -c copy -an -f segment -segment_time ${SEGTIME} -segment_atclocktime 1 -reset_timestamps 1 -strftime 1 -segment_format mp4 "${RAMDIR}/${HOST}-%Y%m%d-%H%M%S.mp4"
 done
